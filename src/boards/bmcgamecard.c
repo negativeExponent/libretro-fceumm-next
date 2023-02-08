be2882e4d6620690d8f3eb20d46f45746e0d3427
negativeExponent <negativeExponent@users.noreply.github.com>
2023-02-08 15:04:28 +0800

    Rewrite mapper 337 and 350

--- src/boards/bmcgamecard.c
+++ src/boards/bmcgamecard.c
@@ -2,6 +2,7 @@
  *
  * Copyright notice for this file:
  *  Copyright (C) 2019 Libretro Team
+ *  Copyright (C) 2023
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,46 +30,79 @@
 
 #include "mapinc.h"
 
-static uint8 latche, m350;
+static uint8 isM350;
+static uint8 latche;
 
-static SFORMAT StateRegs[] =
-{
-	{ &latche, 1, "LATC" },
-	{ &m350, 1, "M350" },
+static SFORMAT StateRegs[] = {
+	{ &latche, 1, "REG0" },
+	{ &isM350, 1, "M350" },
 	{ 0 }
 };
 
 static void Sync(void) {
-	uint8 mirroring = m350 ? ((latche >> 7) & 1) : ((latche >> 5) & 1);
-	uint8 mode = m350 ? ((latche >> 5) & 0x03) : ((latche >> 6) & 0x03);
-	uint8 base = m350 ? ((latche & 0x40) ? (latche & 0x20) : 0) : 0;
+	uint8 mirr, mode, chrp;
+	uint8 bank = latche & 0x1F;
 
-	setchr8(0);
+	if (isM350) {
+		mirr = ((latche >> 7) & 1);
+		mode = ((latche >> 5) & 3);
+		chrp = (latche & 0x40) != 0;
+		if (mode == 3) {
+			/* 2nd chip only has 128KB */
+			bank &= 7;
+			bank |= 0x20;
+		}
+	} else {
+		mirr = ((latche >> 5) & 1);
+		mode = ((latche >> 6) & 3);
+		chrp = (latche & 0x80) != 0;
+	}
+
 	setprg8(0x6000, 1);
-	setprg16r(0, 0x8000, base | (latche & 0x1F));
-	setprg16r(0, 0xC000, base | ((latche & 0x1F) | ((mode & 2) ? 0x07 : (mode & 1))));
-	setmirror(mirroring ^ 1);
+	switch (mode) {
+	case 0: /* NROM-128 */
+		setprg16(0x8000, bank);
+		setprg16(0xC000, bank);
+		break;
+	case 1: /* NROM-256 */
+		setprg32(0x8000, bank >> 1);
+		break;
+	case 2:
+	case 3: /* UNROM */
+		setprg16(0x8000, bank);
+		setprg16(0xC000, bank | 7);
+		break;
+	}
+
+	/* CHR-RAM Protect... kinda */
+	SetupCartCHRMapping(0, CHRptr[0], 0x2000, chrp);
+	setchr8(0);
+	if (isM350 && (latche & 0x40)) {
+		/* use default mirroring */
+	} else {
+		setmirror(mirr ^ 1);
+	}
 }
 
-static DECLFW(BMCCTC12IN1Write8) {
+static DECLFW(GCOuterBankWrite) {
 	latche = (latche & 7) | (V & ~7);
 	Sync();
 }
 
-static DECLFW(BMCCTC12IN1WriteC) {
+static DECLFW(GCInnerBankWrite) {
 	latche = (latche & ~7) | (V & 7);
 	Sync();
 }
 
-static void BMCCTC12IN1Power(void) {
+static void GCPower(void) {
 	Sync();
 	SetReadHandler(0x6000, 0xFFFF, CartBR);
-	SetWriteHandler(0x8000, 0xBFFF, BMCCTC12IN1Write8);
-	SetWriteHandler(0xC000, 0xFFFF, BMCCTC12IN1WriteC);
+	SetWriteHandler(0x8000, 0xBFFF, GCOuterBankWrite);
+	SetWriteHandler(0xC000, 0xFFFF, GCInnerBankWrite);
 }
 
-static void BMCCTC12IN1Reset(void) {
-	latche = 0;
+static void GCReset() {
+	latche = 0; /* always reset to menu */
 	Sync();
 }
 
@@ -76,20 +110,18 @@ static void StateRestore(int version) {
 	Sync();
 }
 
-/* Mapper 337 - BMC-CTC-12IN1 */
-void BMCCTC12IN1_Init(CartInfo *info) {
-	m350 = 0;
-	info->Power = BMCCTC12IN1Power;
-	info->Reset = BMCCTC12IN1Reset;
+static void GameCard_Init(CartInfo *info, int m350) {
+	isM350 = m350;
+	info->Power = GCPower;
+	info->Reset = GCReset;
 	GameStateRestore = StateRestore;
-	AddExState(&StateRegs, ~0, 0, 0);
+	AddExState(StateRegs, ~0, 0, NULL);
 }
 
-/* Mapper 350 - BMC-891227 */
+void BMCCTC12IN1_Init(CartInfo *info) {
+	GameCard_Init(info, 0);
+}
+
 void BMC891227_Init(CartInfo *info) {
-	m350 = 1;
-	info->Power = BMCCTC12IN1Power;
-	info->Reset = BMCCTC12IN1Reset;
-	GameStateRestore = StateRestore;
-	AddExState(&StateRegs, ~0, 0, 0);
+	GameCard_Init(info, 1);
 }
