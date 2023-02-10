024ba4ab5a4547714fe197455a72bbe12213f6ba
negativeExponent <negativeExponent@users.noreply.github.com>
2023-02-10 20:58:45 +0800

    Rewrite mapper 449

--- src/boards/mapper449.c
+++ src/boards/mapper449.c
@@ -19,63 +19,46 @@
  */
 
 #include "mapinc.h"
+#include "latch.h"
 
-static uint16 latch_addr;
-static uint8 latch_data;
 static uint8 dipswitch;
 
 static SFORMAT StateRegs[] = {
-	{ &latch_addr, 2, "ADDR" },
-    { &latch_data, 1, "DATA" },
     { &dipswitch,  1, "DIPS" },
     { 0 }
 };
 
 static void Sync(void) {
-	uint8 prgbank = ((latch_addr >> 2) & 0x1F) | ((latch_addr >> 3) & 0x20);
-	if (~latch_addr & 0x080) {
+	uint8 prgbank = ((latch.addr >> 2) & 0x1F) | ((latch.addr >> 3) & 0x20);
+	if (~latch.addr & 0x080) {
 		setprg16(0x8000, prgbank);
 		setprg16(0xC000, prgbank | 7);
 	} else {
-		if (latch_addr & 0x001) {
+		if (latch.addr & 0x001) {
 			setprg32(0x8000, prgbank >> 1);
 		} else {
 			setprg16(0x8000, prgbank);
 			setprg16(0xC000, prgbank);
 		}
 	}
-	setchr8(latch_data);
-    setmirror((((latch_addr >> 1) & 1) ^ 1));
+	setchr8(latch.data);
+    setmirror((((latch.addr >> 1) & 1) ^ 1));
 }
 
 static DECLFR(M449Read) {
-	if (latch_addr & 0x200)
+	if (latch.addr & 0x200) {
 		return CartBR(A | dipswitch);
+	}
 	return CartBR(A);
-}
-
-static DECLFW(M449Write) {
-	latch_data = V;
-	latch_addr = A & 0xFFFF;
-	Sync();
 }
 
 static void M449Reset(void) {
 	dipswitch  = (dipswitch + 1) & 0xF;
-	latch_addr = latch_data = 0;
-	Sync();
-}
-
-static void M449Power(void) {
-	dipswitch = latch_addr = latch_data = 0;
-	Sync();
-	SetReadHandler(0x6000, 0x7FFF, CartBR);
-	SetReadHandler(0x8000, 0xFFFF, M449Read);
-	SetWriteHandler(0x8000, 0xFFFF, M449Write);
+	LatchHardReset();
 }
 
 void Mapper449_Init(CartInfo *info) {
-	info->Power = M449Power;
+	Latch_Init(info, Sync, M449Read, 0, 0);
 	info->Reset = M449Reset;
 	AddExState(StateRegs, ~0, 0, 0);
 }
