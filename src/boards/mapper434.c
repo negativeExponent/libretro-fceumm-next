5880fb86f0c1fb0bdab46615672645969d09cfbe
negativeExponent <negativeExponent@users.noreply.github.com>
2023-02-10 18:52:13 +0800

    Mapper 434: Fix mirroring

--- src/boards/mapper434.c
+++ src/boards/mapper434.c
@@ -3,6 +3,7 @@
  * Copyright notice for this file:
  *  Copyright (C) 2012 CaH4e3
  *  Copyright (C) 2002 Xodnizel
+ *  Copyright (C) 2023
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,46 +23,36 @@
 /* S-009. UNROM plus outer bank register at $6000-$7FFF. */
 
 #include "mapinc.h"
+#include "latch.h"
 
-static uint16 latch;
+static uint8 outer;
 
-static void Mapper434_Sync(void) {
-	setprg16(0x8000, latch);
-	setprg16(0xC000, latch | 7);
+static void Sync(void) {
+	setprg16(0x8000, (outer << 3) | (latch.data & 7));
+	setprg16(0xC000, (outer << 3) | 7);
 	setchr8(0);
-	setmirror((latch >> 8) & 1);
+	setmirror((outer >> 5) & 1);
 }
 
-static DECLFW(Mapper434_WriteOuterBank) {
-	latch = (latch & 7) | (V << 3);
-	Mapper434_Sync();
+static DECLFW(M434WriteOuterBank) {
+	outer = V;
+	Sync();
 }
 
-static DECLFW(Mapper434_WriteInnerBank) {
-	latch = (latch & ~7) | ((V & CartBR(A)) & 7);
-	Mapper434_Sync();
+static void M434Reset(void) {
+	outer = 0;
+	Sync();
 }
 
-static void Mapper434_Reset(void) {
-	latch = 0;
-	Mapper434_Sync();
-}
-
-static void Mapper434_Power(void) {
-	latch = 0;
-	Mapper434_Sync();
-	SetReadHandler(0x8000, 0xFFFF, CartBR);
-	SetWriteHandler(0x6000, 0x7FFF, Mapper434_WriteOuterBank);
-	SetWriteHandler(0x8000, 0xFFFF, Mapper434_WriteInnerBank);
-}
-
-static void StateRestore(int version) {
-	Mapper434_Sync();
+static void M434Power(void) {
+	outer = 0;
+	LatchPower();
+	SetWriteHandler(0x6000, 0x7FFF, M434WriteOuterBank);
 }
 
 void Mapper434_Init(CartInfo *info) {
-	info->Reset = Mapper434_Reset;
-	info->Power = Mapper434_Power;
-	GameStateRestore = StateRestore;
-	AddExState(&latch, 2, 0, "LATC");
+	Latch_Init(info, Sync, NULL, 0, 1);
+	info->Reset = M434Reset;
+	info->Power = M434Power;
+	AddExState(&outer, 2, 0, "OUTB");
 }
