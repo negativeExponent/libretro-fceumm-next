1a74044a2cf003ef6a2e464734ffb5d5d30684e6
negativeExponent <negativeExponent@users.noreply.github.com>
2023-05-19 21:57:49 +0800

    Add mapper 398

--- src/ines.h
+++ src/ines.h
@@ -306,6 +306,7 @@ void Mapper394_Init(CartInfo *);
 void Mapper395_Init(CartInfo *);
 void Mapper396_Init(CartInfo *);
 void Mapper397_Init(CartInfo *);
+void Mapper398_Init(CartInfo *);
 void Mapper400_Init(CartInfo *);
 void Mapper401_Init(CartInfo *);
 void Mapper402_Init(CartInfo *);
