783b9d0ee9a4823d36933171046b36d6f2eafeb5
negativeExponent <negativeExponent@users.noreply.github.com>
2023-05-19 21:57:49 +0800

    Add mapper 398

--- src/boards/mapper398.c
+++ src/boards/mapper398.c
@@ -18,13 +18,13 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-/* NES 2.0 Mapper 362 - PCB 830506C
- * 1995 Super HiK 4-in-1 (JY-005)
+/* NES 2.0 Mapper 398 - PCB YY840820C
+ * 1995 Super HiK 5-in-1 - 新系列米奇老鼠組合卡 (JY-048)
  */
 
 #include "mapinc.h"
 
-static uint8 game;
+static uint8 latch;
 static uint8 PPUCHRBus;
 static uint8 vrc4Prg[2];
 static uint8 vrc4Mirr;
@@ -36,38 +36,41 @@ static uint8 vrc4IRQCount;
 static int16 vrc4IRQCycles;
 
 static SFORMAT StateRegs[] = {
-    { &game,          1, "GAME" },
-    { &PPUCHRBus,     1, "CHRB" },
-	{ vrc4Prg,        2, "V4PR" },
-	{ &vrc4Mirr,      1, "V4MI" },
-	{ &vrc4Misc,      1, "V4MS" },
-	{ vrc4Chr,       16, "V4CH" },
-	{ &vrc4IRQLatch,  1, "VILA" },
-	{ &vrc4IRQa,      1, "VIMO" },
-	{ &vrc4IRQCount,  1, "VICO" },
+	{ &latch, 1, "GAME" },
+	{ &PPUCHRBus, 1, "CHRB" },
+	{ vrc4Prg, 2, "V4PR" },
+	{ &vrc4Mirr, 1, "V4MI" },
+	{ &vrc4Misc, 1, "V4MS" },
+	{ vrc4Chr, 16, "V4CH" },
+	{ &vrc4IRQLatch, 1, "VILA" },
+	{ &vrc4IRQa, 1, "VIMO" },
+	{ &vrc4IRQCount, 1, "VICO" },
 	{ &vrc4IRQCycles, 2, "VICY" },
 	{ 0 },
 };
 
 static void Sync(void) {
-    uint32 cbase = (vrc4Misc << 13) & 0x4000;
-	uint32 prgBase = (game == 0) ? ((vrc4Chr[PPUCHRBus] & 0x180) >> 3) : 0x40;
-    uint32 chrBase = (game == 0) ? (vrc4Chr[PPUCHRBus] & 0x180) : 0x200;
-	uint32 chrMask = (game == 0) ? 0x7F : 0x1FF;
+	if (latch & 0x80) {
+        /* GNROM-like */
+		setprg32(0x8000, ((latch >> 5) & 0x06) | ((vrc4Chr[PPUCHRBus] >> 2) & 0x01));
+		setchr8(0x40 | ((latch >> 3) & 0x08) | (vrc4Chr[PPUCHRBus] & 0x07));
+	} else {
+		uint32 cbase = (vrc4Misc << 13) & 0x4000;
 
-	setprg8(0x8000 ^ cbase, prgBase | (vrc4Prg[0] & 0x0F));
-	setprg8(0xA000,         prgBase | (vrc4Prg[1] & 0x0F));
-	setprg8(0xC000 ^ cbase, prgBase | ((~1) & 0x0F));
-	setprg8(0xE000,         prgBase | ((~0) & 0x0F));
+		setprg8(0x8000 ^ cbase, (vrc4Prg[0] & 0x0F));
+		setprg8(0xA000,         (vrc4Prg[1] & 0x0F));
+		setprg8(0xC000 ^ cbase, ((~1) & 0x0F));
+		setprg8(0xE000,         ((~0) & 0x0F));
 
-	setchr1(0x0000, chrBase | (vrc4Chr[0] & chrMask));
-	setchr1(0x0400, chrBase | (vrc4Chr[1] & chrMask));
-	setchr1(0x0800, chrBase | (vrc4Chr[2] & chrMask));
-	setchr1(0x0C00, chrBase | (vrc4Chr[3] & chrMask));
-	setchr1(0x1000, chrBase | (vrc4Chr[4] & chrMask));
-	setchr1(0x1400, chrBase | (vrc4Chr[5] & chrMask));
-	setchr1(0x1800, chrBase | (vrc4Chr[6] & chrMask));
-	setchr1(0x1C00, chrBase | (vrc4Chr[7] & chrMask));
+		setchr1(0x0000, (vrc4Chr[0] & 0x1FF));
+		setchr1(0x0400, (vrc4Chr[1] & 0x1FF));
+		setchr1(0x0800, (vrc4Chr[2] & 0x1FF));
+		setchr1(0x0C00, (vrc4Chr[3] & 0x1FF));
+		setchr1(0x1000, (vrc4Chr[4] & 0x1FF));
+		setchr1(0x1400, (vrc4Chr[5] & 0x1FF));
+		setchr1(0x1800, (vrc4Chr[6] & 0x1FF));
+		setchr1(0x1C00, (vrc4Chr[7] & 0x1FF));
+	}
 
 	switch (vrc4Mirr & 0x03) {
 	case 0: setmirror(MI_V); break;
@@ -79,12 +82,12 @@ static void Sync(void) {
 
 static DECLFW(writeVRC4) {
 	uint8 index;
+    latch = A & 0xFF;
 	A &= 0xF003;
 	switch (A & 0xF000) {
 	case 0x8000:
 	case 0xA000:
 		vrc4Prg[(A >> 13) & 1] = V;
-		Sync();
 		break;
 	case 0x9000:
 		if (~A & 2) {
@@ -92,16 +95,11 @@ static DECLFW(writeVRC4) {
 		} else if (~A & 1) {
 			vrc4Misc = V;
 		}
-		Sync();
 		break;
 	case 0xF000:
 		switch (A & 3) {
-		case 0:
-			vrc4IRQLatch = (vrc4IRQLatch & 0xF0) | (V & 0x0F);
-			break;
-		case 1:
-			vrc4IRQLatch = (vrc4IRQLatch & 0x0F) | (V << 4);
-			break;
+		case 0: vrc4IRQLatch = (vrc4IRQLatch & 0xF0) | (V & 0x0F); break;
+		case 1: vrc4IRQLatch = (vrc4IRQLatch & 0x0F) | (V << 4); break;
 		case 2:
 			vrc4IRQa = V;
 			if (vrc4IRQa & 0x02) {
@@ -111,7 +109,7 @@ static DECLFW(writeVRC4) {
 			X6502_IRQEnd(FCEU_IQEXT);
 			break;
 		case 3:
-			/* vrc4IRQa = (vrc4IRQa & ~0x02) | ((vrc4IRQa << 1) & 0x02); */
+			vrc4IRQa = (vrc4IRQa & ~0x02) | ((vrc4IRQa << 1) & 0x02);
 			X6502_IRQEnd(FCEU_IQEXT);
 			break;
 		}
@@ -123,12 +121,13 @@ static DECLFW(writeVRC4) {
 		} else {
 			vrc4Chr[index] = (vrc4Chr[index] & ~0x0F) | (V & 0x0F);
 		}
-		Sync();
 		break;
 	}
+
+    Sync();
 }
 
-static void FP_FASTAPASS(1) M362CPUHook(int a) {
+static void FP_FASTAPASS(1) M398CPUHook(int a) {
 	int count = a;
 	while (count--) {
 		if ((vrc4IRQa & 0x02) && ((vrc4IRQa & 0x04) || ((vrc4IRQCycles -= 3) <= 0))) {
@@ -143,24 +142,20 @@ static void FP_FASTAPASS(1) M362CPUHook(int a) {
 	}
 }
 
-static void FP_FASTAPASS(1) M362PPUHook(uint32 A) {
-    uint8 bank = (A & 0x1FFF) >> 10;
-	if ((game == 0) && (PPUCHRBus != bank) && ((A & 0x3000) != 0x2000)) {
+static void FP_FASTAPASS(1) M398PPUHook(uint32 A) {
+	uint8 bank = (A & 0x1FFF) >> 10;
+	if ((PPUCHRBus != bank) && ((A & 0x3000) != 0x2000)) {
 		PPUCHRBus = bank;
 		Sync();
 	}
 }
 
-static void M362Reset(void) {
-	if (PRGsize[0] <= (512 * 1024)) {
-        game = 0;
-    } else {
-        game = (game + 1) & 0x01;
-    }
+static void M398Reset(void) {
+	latch = 0xC0;
 	Sync();
 }
 
-static void M362Power(void) {
+static void M398Power(void) {
 	int i;
 
 	for (i = 0; i < 2; i++) {
@@ -170,8 +165,9 @@ static void M362Power(void) {
 		vrc4Chr[i] = 0;
 	}
 	vrc4Mirr = vrc4Misc = vrc4IRQLatch = vrc4IRQa = vrc4IRQCount = vrc4IRQCycles = 0;
-	PPUCHRBus = game = 0;
-    Sync();
+	PPUCHRBus = 0;
+    latch = 0xC0;
+	Sync();
 
 	SetReadHandler(0x8000, 0xFFFF, CartBR);
 	SetWriteHandler(0x8000, 0xFFFF, writeVRC4);
@@ -181,11 +177,11 @@ static void StateRestore(int version) {
 	Sync();
 }
 
-void Mapper362_Init(CartInfo *info) {
-	info->Reset = M362Reset;
-	info->Power = M362Power;
-	MapIRQHook = M362CPUHook;
-    PPU_hook = M362PPUHook;
+void Mapper398_Init(CartInfo *info) {
+	info->Reset = M398Reset;
+	info->Power = M398Power;
+	MapIRQHook = M398CPUHook;
+	PPU_hook = M398PPUHook;
 	GameStateRestore = StateRestore;
 	AddExState(StateRegs, ~0, 0, 0);
 }
