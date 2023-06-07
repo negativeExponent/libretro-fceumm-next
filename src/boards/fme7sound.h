7aa4b4adabe6664501d91a3a39144dcd45e98364
negativeExponent <negativeExponent@users.noreply.github.com>
2023-06-07 16:12:01 +0800

    Rewrite sunsoft fme7 mapper

---
+++ src/boards/fme7sound.h
@@ -0,0 +1,12 @@
+#ifndef _FME7_SOUND_H
+#define _FME7_SOUND_H
+
+#include "mapinc.h"
+
+void FME7Sound_ESI(void);
+
+DECLFW(FME7Sound_WriteIndex);
+DECLFW(FME7Sound_WriteCMD);
+
+
+#endif /* _FME7_SOUND_H */
--- src/boards/mapper069.c
+++ src/boards/mapper069.c
@@ -3,6 +3,7 @@
  * Copyright notice for this file:
  *  Copyright (C) 2012 CaH4e3
  *  Copyright (C) 2002 Xodnizel
+ *  Copyright (C) 2023
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,15 +20,13 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "mapinc.h"
+#include "fme7sound.h"
 
 static uint8 cmdreg, preg[4], creg[8], mirr;
 static uint8 IRQa;
 static int32 IRQCount;
 static uint8 *WRAM = NULL;
 static uint32 WRAMSIZE;
-
-static void(*sfun[3]) (void);
 
 static SFORMAT StateRegs[] =
 {
@@ -72,11 +71,11 @@ static DECLFR(M069WRAMRead) {
 		return CartBR(A);
 }
 
-static DECLFW(M069Write0) {
+static DECLFW(M069WriteIndex) {
 	cmdreg = V & 0xF;
 }
 
-static DECLFW(M069Write1) {
+static DECLFW(M069WriteCMD) {
 	switch (cmdreg) {
 		case 0x0:
 			creg[0] = V;
@@ -145,192 +144,18 @@ static DECLFW(M069Write1) {
 	}
 }
 
-/* SUNSOFT-5/FME-7 Sound */
-
-static void AYSound(int Count);
-static void AYSoundHQ(void);
-static void DoAYSQ(int x);
-static void DoAYSQHQ(int x);
-
-static uint8 sndcmd, sreg[14];
-static int32 vcount[3];
-static int32 dcount[3];
-static int32 CAYBC[3];
-
-static SFORMAT SStateRegs[] =
-{
-	{ &sndcmd, 1, "SCMD" },
-	{ sreg, 14, "SREG" },
-	{ dcount, 12, "DCNT" },
-	{ vcount, 12, "VCNT" },
-	{ CAYBC, 12, "BC00" },
-
-	{ 0 }
-};
-
-static DECLFW(M069SWrite0) {
-	sndcmd = V % 14;
-}
-
-static DECLFW(M069SWrite1) {
-	GameExpSound.Fill = AYSound;
-	GameExpSound.HiFill = AYSoundHQ;
-	switch (sndcmd) {
-		case 0:
-		case 1:
-		case 8:
-			if (sfun[0])
-				sfun[0]();
-			break;
-		case 2:
-		case 3:
-		case 9:
-			if (sfun[1])
-				sfun[1]();
-			break;
-		case 4:
-		case 5:
-		case 10:
-			if (sfun[2])
-				sfun[2]();
-			break;
-		case 7:
-			if (sfun[0])
-				sfun[0]();
-			if (sfun[1])
-				sfun[1]();
-			break;
-	}
-	sreg[sndcmd] = V;
-}
-
-static void DoAYSQ(int x) {
-	int32 freq = ((sreg[x << 1] | ((sreg[(x << 1) + 1] & 15) << 8)) + 1) << (4 + 17);
-	int32 amp = (sreg[0x8 + x] & 15) << 2;
-	int32 start, end;
-	int V;
-
-	amp += amp >> 1;
-
-	start = CAYBC[x];
-	end = (SOUNDTS << 16) / soundtsinc;
-	if (end <= start)
-		return;
-	CAYBC[x] = end;
-
-	if (amp && !(sreg[0x7] & (1 << x)))
-		for (V = start; V < end; V++) {
-			if (dcount[x])
-				Wave[V >> 4] += amp;
-			vcount[x] -= nesincsize;
-			while (vcount[x] <= 0) {
-				dcount[x] ^= 1;
-				vcount[x] += freq;
-			}
-		}
-}
-
-static void DoAYSQHQ(int x) {
-	uint32 V;
-	int32 freq = ((sreg[x << 1] | ((sreg[(x << 1) + 1] & 15) << 8)) + 1) << 4;
-	int32 amp = (sreg[0x8 + x] & 15) << 6;
-
-	amp += amp >> 1;
-
-	if (!(sreg[0x7] & (1 << x))) {
-		for (V = CAYBC[x]; V < SOUNDTS; V++) {
-			if (dcount[x])
-				WaveHi[V] += amp;
-			vcount[x]--;
-			if (vcount[x] <= 0) {
-				dcount[x] ^= 1;
-				vcount[x] = freq;
-			}
-		}
-	}
-	CAYBC[x] = SOUNDTS;
-}
-
-static void DoAYSQ1(void) {
-	DoAYSQ(0);
-}
-
-static void DoAYSQ2(void) {
-	DoAYSQ(1);
-}
-
-static void DoAYSQ3(void) {
-	DoAYSQ(2);
-}
-
-static void DoAYSQ1HQ(void) {
-	DoAYSQHQ(0);
-}
-
-static void DoAYSQ2HQ(void) {
-	DoAYSQHQ(1);
-}
-
-static void DoAYSQ3HQ(void) {
-	DoAYSQHQ(2);
-}
-
-static void AYSound(int Count) {
-	int x;
-	DoAYSQ1();
-	DoAYSQ2();
-	DoAYSQ3();
-	for (x = 0; x < 3; x++)
-		CAYBC[x] = Count;
-}
-
-static void AYSoundHQ(void) {
-	DoAYSQ1HQ();
-	DoAYSQ2HQ();
-	DoAYSQ3HQ();
-}
-
-static void AYHiSync(int32 ts) {
-	int x;
-
-	for (x = 0; x < 3; x++)
-		CAYBC[x] = ts;
-}
-
-void Mapper69_ESI(void) {
-	GameExpSound.RChange = Mapper69_ESI;
-	GameExpSound.HiSync = AYHiSync;
-	memset(dcount, 0, sizeof(dcount));
-	memset(vcount, 0, sizeof(vcount));
-	memset(CAYBC, 0, sizeof(CAYBC));
-	if (FSettings.SndRate) {
-		if (FSettings.soundq >= 1) {
-			sfun[0] = DoAYSQ1HQ;
-			sfun[1] = DoAYSQ2HQ;
-			sfun[2] = DoAYSQ3HQ;
-		} else {
-			sfun[0] = DoAYSQ1;
-			sfun[1] = DoAYSQ2;
-			sfun[2] = DoAYSQ3;
-		}
-	} else
-		memset(sfun, 0, sizeof(sfun));
-}
-
-/* SUNSOFT-5/FME-7 Sound */
-
 static void M069Power(void) {
-	cmdreg = sndcmd = 0;
+	cmdreg = 0;
 	IRQCount = 0xFFFF;
 	IRQa = 0;
 	Sync();
 	SetReadHandler(0x6000, 0x7FFF, M069WRAMRead);
 	SetWriteHandler(0x6000, 0x7FFF, M069WRAMWrite);
 	SetReadHandler(0x8000, 0xFFFF, CartBR);
-	SetWriteHandler(0x8000, 0x9FFF, M069Write0);
-	SetWriteHandler(0xA000, 0xBFFF, M069Write1);
-	SetWriteHandler(0xC000, 0xDFFF, M069SWrite0);
-	SetWriteHandler(0xE000, 0xFFFF, M069SWrite1);
+	SetWriteHandler(0x8000, 0x9FFF, M069WriteIndex);
+	SetWriteHandler(0xA000, 0xBFFF, M069WriteCMD);
+	SetWriteHandler(0xC000, 0xDFFF, FME7Sound_WriteIndex);
+	SetWriteHandler(0xE000, 0xFFFF, FME7Sound_WriteCMD);
 	FCEU_CheatAddRAM(WRAMSIZE >> 10, 0x6000, WRAM);
 }
 
@@ -368,15 +193,12 @@ void Mapper069_Init(CartInfo *info) {
 		info->SaveGameLen[0] = WRAMSIZE;
 	}
 	GameStateRestore = StateRestore;
-	Mapper69_ESI();
+	FME7Sound_ESI();
 	AddExState(&StateRegs, ~0, 0, 0);
-	AddExState(&SStateRegs, ~0, 0, 0);
 }
 
 void NSFAY_Init(void) {
-	sndcmd = 0;
-	SetWriteHandler(0xC000, 0xDFFF, M069SWrite0);
-	SetWriteHandler(0xE000, 0xFFFF, M069SWrite1);
-	Mapper69_ESI();
-	AddExState(&SStateRegs, ~0, 0, 0);
+	SetWriteHandler(0xC000, 0xDFFF, FME7Sound_WriteIndex);
+	SetWriteHandler(0xE000, 0xFFFF, FME7Sound_WriteCMD);
+	FME7Sound_ESI();
 }
