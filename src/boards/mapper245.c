24e161a9169545816a431674264b251f1b19d5a3
negativeExponent <negativeExponent@users.noreply.github.com>
2023-06-17 11:49:52 +0800

    Update mapper 245

--- src/boards/mapper245.c
+++ src/boards/mapper245.c
@@ -21,13 +21,38 @@
 #include "mapinc.h"
 #include "mmc3.h"
 
-static void M245CW(uint32 A, uint8 V) {
+static void M245FixCHR(void) {
 	setchr8(0);
-	MMC3_FixPRG();
 }
 
 static void M245PW(uint32 A, uint8 V) {
 	setprg8(A, (V & 0x3F) | ((MMC3GetCHRBank(mmc3.expregs[0]) & 0x02) << 5));
+}
+
+static DECLFW(M245Write) {
+    A &= 0xE001;
+    switch (A) {
+    case 0x8001:
+        switch (mmc3.cmd & 0x07) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            mmc3.regs[mmc3.cmd & 0x07] = V;
+			MMC3_FixPRG();
+			MMC3_FixCHR();
+            break;
+        default:
+            MMC3_CMDWrite(A, V);
+            break;
+        }
+        break;
+    default:
+        MMC3_CMDWrite(A, V);
+        break;
+    }
 }
 
 static void M245PPUHook(uint32 A) {
@@ -44,13 +69,14 @@ static void M245PPUHook(uint32 A) {
 static void M245Power(void) {
 	mmc3.expregs[0] = 0;
 	GenMMC3Power();
+	SetWriteHandler(0x8000, 0x9FFF, M245Write);
 }
 
 void Mapper245_Init(CartInfo *info) {
 	GenMMC3_Init(info, 8, info->battery);
 	info->Power = M245Power;
 	MMC3_pwrap = M245PW;
-	MMC3_cwrap = M245CW;
+	MMC3_FixCHR = M245FixCHR;
 	PPU_hook = M245PPUHook;
 	AddExState(mmc3.expregs, 1, 0, "EXPR");
 }
