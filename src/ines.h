376e4231298be2f9cdd48fb00deea395db726a4c
negativeExponent <negativeExponent@users.noreply.github.com>
2025-09-20 20:05:48 +0800

    Move  m587 to tested list, Add UNIF variant BMC-K-3057

--- src/ines.h
+++ src/ines.h
@@ -512,6 +512,7 @@ void Mapper559_Init(CartInfo *);
 void Mapper561_Init(CartInfo *);
 void Mapper562_Init(CartInfo *);
 void Mapper564_Init(CartInfo *);
+void Mapper587_Init(CartInfo *);
 
 void Mapper273_Init(CartInfo *);
 void Mapper367_Init(CartInfo *);
@@ -574,7 +575,7 @@ void Mapper583_Init(CartInfo *);
 void Mapper584_Init(CartInfo *);
 void Mapper585_Init(CartInfo *);
 void Mapper586_Init(CartInfo *);
-void Mapper587_Init(CartInfo *);
+
 void Mapper588_Init(CartInfo *);
 void Mapper589_Init(CartInfo *);
 void Mapper590_Init(CartInfo *);
--- src/unif.c
+++ src/unif.c
@@ -683,6 +683,7 @@ static BMAPPING bmap[] = {
 	{ "LH53",                       535, 0, Mapper535_Init, 0 },
 	{ "82112C",                     540, 0, Mapper540_Init, 0 },
 	{ "KONAMI-QTAI",                547, 0, Mapper547_Init, 0 },
+	{ "K-3057",                     587, 0, Mapper587_Init, 0 },
 
 	{ "SSS-NROM-256",           NO_INES, 0, SSSNROM_Init,     0 }, /* famicombox - cant find similar cart */
 	{ "T-227-1",                NO_INES, 0, BMCT2271_Init,    0 }, /* cant find similar cart */
